0c78539279feea90f672cef4a71df298d7a7a2a2
Aditya Suman <94547402+adityasuman990@users.noreply.github.com>
2023-04-16 02:00:56 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://adityasuman990:Adityasuman990@cluster0.qagqwyj.mongodb.net/test", 
+mongoose.connect("your database", 
 {useNewUrlParser : true, useUnifiedTopology: true, Family:4})
 .then(()=>{console.log("Connected to MongoDB");}).catch((e)=>{ console.log(e);});
 
